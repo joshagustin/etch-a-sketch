f96e47f75f9604322c4907976b85cc774c88d69e
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-18 23:53:47 +0800

    Create functions to color cells on mouse events

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let mouseDown = false;
+createGrid();
+
 function createGrid() {
     const container = document.querySelector('.container');
-    console.log(container);
     for (let i = 0; i < 16; i++) {
         const gridCell = document.createElement('div');
         gridCell.classList.add('gridCell');
@@ -8,4 +10,25 @@ function createGrid() {
     }
 }
 
-createGrid();
+function colorCellClick() {
+    this.style.background = '#00FFFF';
+}
+
+function colorCellMouse() {
+    if (!mouseDown) return;
+    this.style.background = '#00FFFF';
+}
+
+const gridCells = document.querySelectorAll('.gridCell');
+gridCells.forEach(cell => {
+    cell.addEventListener('mousedown', colorCellClick);
+    cell.addEventListener('mouseover', colorCellMouse);
+})
+
+window.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+window.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
