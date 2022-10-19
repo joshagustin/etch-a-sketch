b7fe28fdbb8f2d598207900ee44640ee3e2ba75c
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-19 14:43:07 +0800

    Create methods to change grid size

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let mouseDown = false;
-createGrid();
+const sizeButton = document.querySelector('.size.button');
+createGrid(4);
 
-function createGrid() {
+function createGrid(size) {
     const container = document.querySelector('.container');
-    for (let i = 0; i < 16; i++) {
-        const gridCell = document.createElement('div');
+    // size in px
+    const containerSize = container.offsetWidth;
+    const cellSideLength = containerSize / size;
+    const totalCells = size ** 2
+    for (let i = 0; i < totalCells; i++) {
+        const gridCell = document.createElement('div')
         gridCell.classList.add('gridCell');
+        gridCell.style.width = `${cellSideLength}px`;
+        gridCell.style.height = `${cellSideLength}px`;
         container.appendChild(gridCell);
     }
+    addEventListenerToCells();
 }
 
 function colorCellClick() {
@@ -19,11 +27,38 @@ function colorCellMouse() {
     this.style.background = '#00FFFF';
 }
 
-const gridCells = document.querySelectorAll('.gridCell');
-gridCells.forEach(cell => {
-    cell.addEventListener('mousedown', colorCellClick);
-    cell.addEventListener('mouseover', colorCellMouse);
-})
+function changeGridSize() {
+    const newSize = getGridSize();
+    if (newSize == null) return;
+    deleteGrid();
+    createGrid(newSize);
+}
+
+function getGridSize() {
+    let newSize = '';
+    while (!Number.isInteger(newSize) || newSize < 1 || newSize > 100) {
+        newSize = prompt("Enter an integer (1 - 100): ");
+        if (newSize == null) break;
+        newSize = Number(newSize);
+    }
+    return newSize;
+}
+
+function deleteGrid() {
+    const gridCells = document.querySelectorAll('.gridCell');
+    const container = document.querySelector('.container');;
+    gridCells.forEach(cell => {
+        container.removeChild(cell);
+    });
+}
+
+function addEventListenerToCells() {
+    const gridCells = document.querySelectorAll('.gridCell');
+    gridCells.forEach(cell => {
+        cell.addEventListener('mousedown', colorCellClick);
+        cell.addEventListener('mouseover', colorCellMouse);
+    });
+}
 
 window.addEventListener('mousedown', () => {
     mouseDown = true;
@@ -32,3 +67,5 @@ window.addEventListener('mousedown', () => {
 window.addEventListener('mouseup', () => {
     mouseDown = false;
 });
+
+sizeButton.addEventListener('click', changeGridSize);
